85180110557a02fa5d4028fadc57050548f3ef59
Fernando <fmti91@hotmail.com>
2018-09-10 19:36:05 -0300

    new models and migrations

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,9 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\Model\Product::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
---
+++ database/factories/ReviewFactory.php
@@ -0,0 +1,9 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\Model\Review::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
