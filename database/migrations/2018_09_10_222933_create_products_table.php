19f45a3c556118f0753614883e402ed02c80df8e
Fernando <fmti91@hotmail.com>
2018-09-10 19:55:03 -0300

    update migrations and routes

--- database/migrations/2018_09_10_222933_create_products_table.php
+++ database/migrations/2018_09_10_222933_create_products_table.php
@@ -15,6 +15,11 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('name');
+            $table->text('detail');
+            $table->double('price');
+            $table->integer('stock');
+            $table->double('discount');
             $table->timestamps();
         });
     }
--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,11 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+//php artisan route:list
+//products
+Route::apiResource('/products', 'ProductController');
+//reviews products
+Route::group(['prefix' => 'products'], function(){
+  Route::apiResource('/{product}/reviews', 'ReviewController');
+});
