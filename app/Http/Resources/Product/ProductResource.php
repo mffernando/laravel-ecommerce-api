80b4d2511e469d5436bf7e8b2cdb3bfdd5117c5b
Fernando <fmti91@hotmail.com>
2018-09-11 08:56:54 -0300

    update the ProductResource

--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -18,8 +18,23 @@ class ProductResource extends JsonResource
           'name' => $this->name,
           'description' => $this->detail,
           'price' => $this->price,
-          'stock' => $this->stock,
+          'stock' => $this->stock == 0 ? 'Out of stock' : $this->stock,
           'discount' => $this->discount,
+          //price with discount
+          //example:
+          //17/100 = .17 ($this->discount/100)
+          //1 - .17 = .83 ((1 - ($this->discount/100))
+          //.83 * 404 = 335,32 ((1 - ($this->discount/100)) * $this->price, 2)
+          'totalPrice' => round((1 - ($this->discount/100)) * $this->price, 2),
+          //star sum rating divided by number of reviews
+          //round a number to the nearest integer
+          //remove / 0
+          'rating' => $this->reviews->count() > 0 ? round($this->reviews->sum('star')/
+                      $this->reviews->count(), 1) : 'No rating yet',
+          //get reviews by id
+          'href' => [
+            'reviews' => route('reviews.index', $this->id),
+          ]
         ];
     }
 }
