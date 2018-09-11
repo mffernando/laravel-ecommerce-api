1e8918b42f6d3bec3129b9fb76a59e0d5c08b51a
Fernando <fmti91@hotmail.com>
2018-09-10 21:26:36 -0300

    relationship between models

--- app/Model/Product.php
+++ app/Model/Product.php
@@ -3,8 +3,12 @@
 namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Model\Review;
 
 class Product extends Model
 {
-    //
+    public function reviews()
+    {
+      return $this->hasMany(Review::class);
+    }
 }
