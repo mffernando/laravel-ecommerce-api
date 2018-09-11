c7fecc45089562886acde9ebf93b9d5e91377c86
Fernando <fmti91@hotmail.com>
2018-09-11 09:25:04 -0300

    update ProductCollection and ProductController

--- app/Http/Resources/Product/ProductCollection.php
+++ app/Http/Resources/Product/ProductCollection.php
@@ -2,9 +2,9 @@
 
 namespace App\Http\Resources\Product;
 
-use Illuminate\Http\Resources\Json\ResourceCollection;
+use Illuminate\Http\Resources\Json\Resource;
 
-class ProductCollection extends ResourceCollection
+class ProductCollection extends Resource
 {
     /**
      * Transform the resource collection into an array.
@@ -14,6 +14,15 @@ class ProductCollection extends ResourceCollection
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+          'name' => $this->name,
+          'totalPrice' => round((1 - ($this->discount/100)) * $this->price, 2),
+          'rating' => $this->reviews->count() > 0 ? round($this->reviews->sum('star')/
+                      $this->reviews->count(), 1) : 'No rating yet',
+          'discount' => $this->discount,
+          'href' => [
+            'link' => route('products.show', $this->id)
+          ],
+        ];
     }
 }
