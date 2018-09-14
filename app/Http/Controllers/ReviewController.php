15ba1eee2397b123fb7457b9c5c461227fbb915e
Fernando <fmti91@hotmail.com>
2018-09-14 12:34:47 -0300

    update and delete review

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -75,9 +75,14 @@ class ReviewController extends Controller
      * @param  \App\Model\Review  $review
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Review $review)
+    public function update(Request $request, Product $product, Review $review)
     {
-        //
+        //return $review;
+        //return $product;
+        $review->update($request->all());
+        return response([
+          'data' => new ReviewResource($review)
+        ], 201);
     }
 
     /**
@@ -86,8 +91,9 @@ class ReviewController extends Controller
      * @param  \App\Model\Review  $review
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Review $review)
+    public function destroy(Product $product, Review $review)
     {
-        //
+        $review->delete();
+        return response(null, 204);
     }
 }
--- app/Http/Resources/ReviewResource.php
+++ app/Http/Resources/ReviewResource.php
@@ -15,6 +15,7 @@ class ReviewResource extends JsonResource
     public function toArray($request)
     {
         return [
+          'id' => $this->id,
           'customer' => $this->customer,
           'review' => $this->review,
           'star' => $this->star,
